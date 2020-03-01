191f7c7e415efe7172cd874e1cb5d472e3c23c52
Joep Kockelkorn <joep.kockelkorn@engie.com>
2020-03-01 19:49:39 +0100

    feat: move versions config to json

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,25 @@
 const versionPicker = document.getElementById('version-picker');
 const iframe = document.getElementById('iframe');
 
-// by default pick first version?
-const defaultVersion = versionPicker.children.length ? versionPicker.children[0] : null;
-if (defaultVersion !== null) {
-  iframe.setAttribute('src', defaultVersion.value);
-}
+// get versions
+fetch('docs/versions.json').then(async res => {
+  const versions = await res.json();
+  let first = true;
+  for (const [version, location] of Object.entries(versions)) {
+    const option = new Option(version, location);
+    versionPicker.add(option);
+    first = false;
+  }
 
-// listen for changes
-versionPicker.addEventListener('change', ({ target: { value } }) => {
-  const docsPath = value ? value : 'no-results.html';
-  iframe.setAttribute('src', docsPath);
+  // by default pick first version
+  const defaultVersion = versionPicker.children.length ? versionPicker.children[0] : null;
+  if (defaultVersion !== null) {
+    iframe.setAttribute('src', defaultVersion.value);
+  }
+
+  // listen for changes
+  versionPicker.addEventListener('change', ({ target: { value } }) => {
+    const docsPath = value ? value : 'no-results.html';
+    iframe.setAttribute('src', docsPath);
+  });
 });
